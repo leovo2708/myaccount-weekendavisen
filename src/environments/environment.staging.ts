894a9ee1ff90be4b4e4b165e3274ef3c3ba14d7e
Daniel Kokott <dako@berlingskemedia.dk>
2017-05-09 10:46:30 +0200

    apiUrl for new AWS+Puppet setup

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,7 +2,7 @@ import IEnvironment from './environment.interface';
 
 export const environment: IEnvironment = {
   production: true,
-  apiUrl: 'https://myaccount.weekendavisen-staging.dk:8000',
+  apiUrl: 'https://myaccount.weekendavisen-staging.dk:9000',
   TID: '3',
   brand: 'weekendavisen'
 };
