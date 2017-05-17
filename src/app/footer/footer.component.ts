989cade008a4159751d5c4a436365ff904dba4b5
Sergey Shalapuda <xsesh@berlingskemedia.dk>
2017-05-17 10:03:27 +0200

    UTIL-1472 footer data moved to external configuration (#22)

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { footerData } from './footer.data';
+import { environment } from '../../environments/environment';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  footerData = footerData[environment.brand];
   isCookiePolicySubmitted: boolean = this.getCookiePolicy();
 
   getCookiePolicy(): boolean {
---
+++ src/app/footer/footer.data.ts
@@ -0,0 +1,73 @@
+export const footerData = {
+  weekendavisen: {
+    columns: [
+      {
+        title: {title: 'Forside', link: 'http://www.weekendavisen.dk'},
+      },
+      {
+        title: {link: 'http://www.weekendavisen.dk/side/avisen', title: 'Avisen'},
+        items: [
+          {link: 'http://www.weekendavisen.dk/side/avisen', title: 'Overblik'},
+          {link: 'http://www.weekendavisen.dk/lydavis', title: 'Lydavis'},
+          {link: 'http://www.weekendavisen.dk/e-avis', title: 'E-Avis'},
+          {link: 'http://www.weekendavisen.dk/side/arkiver', title: 'Arkiv'},
+          {link: 'http://www.weekendavisen.dk/side/penkowa-sagen', title: 'Penkowa-sagen'}
+        ]
+      },
+      {
+        title: {link: 'http://www.weekendavisen.dk/bog', title: 'Bøger'},
+        items: [
+          {link: 'http://www.weekendavisen.dk/bog', title: 'Aktuelle'},
+          {link: 'http://www.weekendavisen.dk/bog/find/0/a', title: 'Søg bog'},
+          {link: 'http://www.weekendavisen.dk/bog/alle', title: 'Oversigt'},
+          {link: 'http://www.weekendavisen.dk/bog/anmelderliste', title: 'Anmeldere'},
+          {link: 'http://www.weekendavisen.dk/litteraturpriser', title: 'Litteraturpris'}
+        ]
+      },
+      {
+        title: {link: 'http://www.weekendavisen.dk/side/liv', title: 'Liv'},
+        items: [
+          {link: 'http://www.weekendavisen.dk/side/liv', title: 'Om LIV'},
+          {link: 'http://www.weekendavisen.dk/rejse/liste', title: 'Læserrejser'},
+          {link: 'http://www.weekendavisen.dk/side/vinklub', title: 'Læsernes Vinklub'},
+          {link: 'http://www.filmporten.dk', title: 'Læsernes filmklub'},
+          {link: 'http://www.weekendavisen.dk/side/skagenfood', title: 'Skagenfood'}
+        ]
+      },
+      {
+        title: {link: 'http://www.weekendavisen.dk/side/om-os', title: 'Om os'},
+        items: [
+          {link: 'http://www.weekendavisen.dk/side/om-os', title: 'Historien'},
+          {link: 'http://www.weekendavisen.dk/medarbejderliste', title: 'Medarbejdere'},
+          {link: 'http://www.weekendavisen.dk/skribent/liste', title: 'Bogudgivelser'},
+          {link: 'http://www.weekendavisen.dk/side/kontakt-os', title: 'Kontakt os'},
+          {link: 'http://www.berlingskemedia.dk/?brands=weekendavisen', title: 'Annoncer'}
+        ]
+      },
+      {
+        title: {link: 'http://www.weekendavisen.dk/side/abonnement', title: 'Abonnement'},
+        items: [
+          {link: 'http://www.weekendavisenkampagne.dk/indland', title: 'Indland'},
+          {link: 'http://www.weekendavisenkampagne.dk/udland', title: 'Udland'},
+          {link: 'http://weekendavisen.kathlab.dk/studierabat/', title: 'Studerende'},
+          {link: 'http://www.weekendavisenkampagne.dk/XXX', title: 'Abonnement på E-avisen'},
+          {link: 'http://www.weekendavisenkampagne.dk/gave', title: 'Gaveabonnement'},
+          {link: 'https://weekendavisen.kundeunivers.dk/', title: 'Kundeunivers'},
+          {link: 'http://www.weekendavisen.dk/side/weekendavisens-udgivelseskalender', title: 'Udgivelseskalender'}
+        ]
+      }
+    ],
+    infoBox: [
+      {link: 'http://www.berlingskemedia.dk/ophavsret-og-vilkaar/', title: 'Ophavsret'},
+      {link: 'http://www.berlingskemedia.dk/cookie-og-privatlivspolitik/', title: 'Privatlivspolitik'},
+      {link: 'http://www.berlingskemedia.dk/generelle-handelsbetingelser/', title: 'Generelle handelsbetingelser'}
+    ],
+    infoBox2: {
+      text: 'Weekendavisen Pilestræde 34 DK-1147 København K. CVR: 25337654 Telefon: 33752533'
+    },
+    cookiesNote: {
+      text: 'Websitet anvender cookies for at huske dine indstillinger, statistik og målrette annoncer. Denne information deles med tredjepart.',
+      rules: {link: 'http://www.berlingskemedia.dk/cookie-og-privatlivspolitik/', title: 'Læs mere'}
+    }
+  }
+};
