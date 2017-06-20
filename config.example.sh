134e14e4e7a2b975e9ff7aaf8d24f0682b85d380
xmaga <xmaga@berlingskemedia.dk>
2017-06-20 11:20:09 +0200

    UTIL-1638 MyAccount - Weekendavisen - helper box component - adjustments after PR comments

--- config.example.sh
+++ config.example.sh
@@ -10,6 +10,10 @@ export BPC_APP_ID=test_sso_app
 export BPC_APP_SECRET=gfkj23h4kjh453kfhfsdkkgfj393jkldl
 export BPC_URL=http://BPC_API_Url
 
+export GIGYA_API_KEY=
+export GIGYA_USER_KEY=
+export GIGYA_SECRET=
+
 export KU_APPLICATION_URL=http://Kundeunivers_API_Url
 
 export MDBAPI_ADDRESS=192.168.0.0
